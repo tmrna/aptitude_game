3f4e1d965f121fec181e6b49b11898500205fb8b
tmrna <r.deans@outlook.com>
2023-08-02 15:28:47 -0400

    fix: was trying to index array rather than accessing params

--- bus_game/components/non_interactable/buffer_box/map_buffer_boxes.jsx
+++ bus_game/components/non_interactable/buffer_box/map_buffer_boxes.jsx
@@ -1,6 +1,6 @@
 import bufferBox from "./buffer_box"
 export default function MapBufferBoxes(boxDetails) {
 	return boxDetails.map( (boxInfo, index) => {
-		return bufferBox(boxInfo[0], boxInfo[1], index);
+		return bufferBox(boxInfo.occupied, boxInfo.color, index);
 	})
 } 
