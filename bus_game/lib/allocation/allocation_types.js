b5ab74eda29e16fafbb30ddc4fe4b703b09f8ef7
tmrna <r.deans@outlook.com>
2023-08-02 17:34:19 -0400

    added enum for color types

--- bus_game/lib/allocation/allocation_types.js
+++ bus_game/lib/allocation/allocation_types.js
@@ -2,3 +2,27 @@ export const AllocationTypes = {
   NonContiguous: "Non-Contiguous",
   Contiguous: "Contiguous",
 }
+
+export const NUM_COLORS = Object.keys(Colors).length;
+
+export const Colors = {
+  fuschia: "fuschia",
+  lightSteelBlue: "lightSteelBlue",
+  royalBlue: "royalBlue",
+  navy: "navy",
+  lime: "lime",
+  cyan: "cyan",
+  chartreuse: "chartreuse",
+  maroon: "maroon",
+  red: "red",
+  purple: "purple",
+  darkViolet: "darkviolet",
+  blue: "blue",
+  orangeRed: "orangeRed",
+  orange: "orange",
+  indigo: "indigo",
+  green: "green",
+  chocolate: "chocolate",
+  brown: "brown",
+  orchid: "orchid",
+}
