bbb987706e9da299fcc7dfe4f3346ec2932a920b
tmrna <r.deans@outlook.com>
2023-08-05 10:51:35 -0400

    Moving away from drag and drop, would slow the game down

--- bus_game/components/non_interactable/view_port/view_port.jsx
+++ bus_game/components/non_interactable/view_port/view_port.jsx
@@ -1,5 +1,8 @@
-import lazyDiv from "../../../lib/utilities/lazy_div";
 import "../../../styles/view_port.css";
 export default function ViewPort({children}) {
-	return lazyDiv("viewPort", children);
+	return (
+		<div className = "viewPort">
+			{children}
+		</div>
+	);
 }
