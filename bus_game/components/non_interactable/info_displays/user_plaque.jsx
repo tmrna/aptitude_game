04c396b3c568c53f6fc9cb7516bde89aa946d143
tmrna <r.deans@outlook.com>
2023-08-07 13:21:05 -0400

    updated userPlaque

--- bus_game/components/non_interactable/info_displays/user_plaque.jsx
+++ bus_game/components/non_interactable/info_displays/user_plaque.jsx
@@ -3,7 +3,7 @@ import { userPlaqueStyle } from "./info_display_styles"
 export default function UserPlaque(username) {
 	return (
 		<div className={userPlaqueStyle}>
-			<h1>{username}</h1>
+			<h1>fd: {username}</h1>
 		</div>
 	)
 }
