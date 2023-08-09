2bfd23d62762339bc2c9da63ebd67e3d506e19a6
tmrna <r.deans@outlook.com>
2023-08-09 19:50:54 -0400

    added workflows for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/aptitude_game/",
   plugins: [react()],
   resolve: {
     alias: {
