66f7f92646c88a3c4f617253417df8d74b0d8582
tmrna <r.deans@outlook.com>
2023-08-09 12:11:26 -0400

    added functions for cookies handling scoreboard

---
+++ bus_game/lib/cookies/score_cookies.js
@@ -0,0 +1,86 @@
+import { deleteCookie, getCookie, setExpireableCookie } from "./cookie_operations";
+import { GUEST_TAG, getUserName } from "./user_cookies";
+
+const SCORE_NAME = "scores";
+const SCORE_DAYS_TILL_EX = 30;
+
+/*
+ * scores=user1:score1,user2:score2,user3:score3....user(n):score(n);
+ */
+
+function replaceScoreString(scoreString){
+	setExpireableCookie(SCORE_NAME, scoreString, SCORE_DAYS_TILL_EX);
+}
+
+export function deserializeScores() {
+	const scoreBoardString = getCookie(SCORE_NAME);
+	const scoreBoardStringArray = scoreBoardString.split(',');
+	const scoreBoardArray = [];
+
+	for(let i = 0; i < scoreBoardStringArray.length; ++i) {
+		const scoreInstance = scoreBoardStringArray[i];
+		var usernameScore = scoreInstance.split(':');
+		if(usernameScore.length === 2) {
+			usernameScore[1] = Number(usernameScore[1]);
+			scoreBoardArray.push(usernameScore);
+		}
+	}
+
+	return scoreBoardArray;
+}
+
+export function pushScore(scoreNumber) {
+	if(isNaN(scoreNumber)) throw new TypeError("score must be a number.");
+
+	var scoreString = getCookie(SCORE_NAME);
+	const username = getUserName();
+
+	if(username === GUEST_TAG) return;
+
+	if(updateScoreCookie(scoreNumber, scoreString, username)) return;
+
+	
+	if(scoreString.length > 0) scoreString += ',';
+	if(username.length > 0){
+		scoreString += username + ':' + scoreNumber.toString();
+		replaceScoreString(scoreString);
+	}
+}
+
+export function updateScoreCookie(newScore, scoreString, username) {
+	const index = scoreString.search(username);
+	if(index === -1) {
+		return false;
+	}
+
+	const usernameOffset = index + username.length + 1;
+	const toTruncate = scoreString.substring(usernameOffset);
+	const endScoreRegion = toTruncate.search(',');
+
+	// at this point we know the username is in the string 
+	// endScoreRegion === -1 means we are at the last score
+	
+	if(endScoreRegion === -1){
+		const oldScore = Number(scoreString.substring(usernameOffset));	
+		if(newScore > oldScore) {
+			var newScoreString = scoreString.substring(0, usernameOffset);
+			newScoreString += newScore.toString();
+			replaceScoreString(newScoreString);
+		}	
+	}
+	else{
+		const oldScore = Number(scoreString.substring(usernameOffset, endScoreRegion));
+		if(newScore > oldScore){
+			var newScoreString = scoreString.substring(0, usernameOffset);
+			const upperRegion = scoreString.substring(endScoreRegion);
+			newScoreString += newScore.toString();
+			newScoreString += upperRegion;
+			replaceScoreString(newScoreString)
+		}
+	}
+	return true;
+}
+
+export function clearScoreBoard() {
+	deleteCookie(SCORE_NAME);
+}
