3649a252dd3b96505ad529a86ce434182b9e1a8c
tmrna <r.deans@outlook.com>
2023-08-09 12:10:40 -0400

    cleanup, added delete and get for username

--- bus_game/lib/cookies/user_cookies.js
+++ bus_game/lib/cookies/user_cookies.js
@@ -1,12 +1,18 @@
-import { cookieExists, getCookie, setExpireableCookie, deleteCookie } from "./cookie_operations";
+import { getCookie, setExpireableCookie, deleteCookie } from "./cookie_operations";
 
 const USERNAME_STRING = "username";
+const DAYS_TILL_UNAME_EX = 3;
 
 export function setUserCookie(username) {
-	if(cookieExists(USERNAME_STRING)) {
-		deleteCookie(USERNAME_STRING);
-	}
-
-	const daysTillEx = 3;
-	setExpireableCookie(USERNAME_STRING, username, daysTillEx);
+	setExpireableCookie(USERNAME_STRING, username, DAYS_TILL_UNAME_EX);
 }
+
+export function deleteUsername() {
+	deleteCookie(USERNAME_STRING);
+}
+
+export function getUserName() {
+	return getCookie(USERNAME_STRING);
+}
+
+export const GUEST_TAG = '$GUEST$';
